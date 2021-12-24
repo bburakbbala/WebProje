8d60b0cceaee1a5aa876b4749230b02eb3376c2e
bburakbbala <burak.bala@gmail.com>
2021-12-25 00:12:45 +0300

    Add project files.

---
+++ Clinic.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clinic.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
